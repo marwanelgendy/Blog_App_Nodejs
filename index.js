44a7ffdff2f7fdc73c4672eeea9e961cccc7adb7
marwanelgendy <marwantarek852@gmail.com>
2022-12-19 20:26:42 +0200

    add template engine

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 // create express app
 const app = express()
 
+// set template engine
+app.set('view engine' , 'ejs')
 // serve static files from public folder
 app.use(express.static('public'))
 
@@ -14,18 +17,18 @@ app.listen(4000 , ()=>{
 
 // create routes
 app.get('/', (req , res)=>{
-    res.sendFile(path.resolve(__dirname + 'pages/index.html'))
+    res.render('index')
 })
 
 app.get('/about', (req , res)=>{
-    res.sendFile(path.resolve(__dirname + 'pages/about.html'))
+    res.render('about')
 })
 
 app.get('/contact', (req , res)=>{
-    res.sendFile(path.resolve(__dirname + 'pages/contact.html'))
+    res.render('contact')
 })
 
 app.get('/post', (req , res)=>{
-    res.sendFile(path.resolve(__dirname + 'pages/post.html'))
+    res.render('post')
 })
 
