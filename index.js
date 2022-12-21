053e2f9c3c59bee78bee4fc61a05c807356c760f
Marwan Elgendy <marwantarek852@gmail.com>
2022-12-21 15:20:20 +0200

    add mongoDB part

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 const ejs = require('ejs')
+const config = require('./config')
 
 // create express app
 const app = express()
+
+// connect to database
+const url = config.mongoUrl
+const connect = mongoose.connect(url)
+
+connect.then(dp =>{
+    console.log("Connect correctly to server")
+}, err => {console.log(err)})
 
 // set template engine
 app.set('view engine' , 'ejs')
@@ -31,4 +41,3 @@ app.get('/contact', (req , res)=>{
 app.get('/post', (req , res)=>{
     res.render('post')
 })
-
